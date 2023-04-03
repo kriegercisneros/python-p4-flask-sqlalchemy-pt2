d30fb4e8bdd4adb3b108e7afb965e96aa07daa80
JackieCisneros <krieger.jacqueline@gmail.com>
2023-04-03 16:48:08 -0600

    finished assignment

--- server/app.py
+++ server/app.py
@@ -1,18 +1,75 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Pet, Owner
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#this avoids building up too much unhelpful data in memory when our application is running 
 
+#configures the application and models for Flask-Migrate
 migrate = Migrate(app, db)
 
+# connects our db to our application before it runs
 db.init_app(app)
 
+#determines which resoures are available at which URLs and saves them to 
+#the application's URL map
+@app.route('/')
+def index():
+    #we return responses to the client after a request.  this one has a status 
+    #code of 200, which means the resource exists an is accessible at the URL
+    response = make_response(
+        '<h1>Welcome to the pet/owner directory!</h1>', 200
+    )
+    return response
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id ==id).first()
+
+    if not pet:
+        response_body = '<h1>404 pet not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+    <h1>Information for {pet.name}</h1>
+    <h2>Pet Species is {pet.species}</h2>
+    <h2>Pet owner is {pet.owner.name}</h2>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
+
+@app.route('/owner/<int:id>')
+def owner_by_id(id):
+    owner = Owner.query.filter(Owner.id ==id).first()
+
+    if not owner:
+        response_body = '<h1>404 owner not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'<h1>Information for {owner.name}</h1>'
+
+    pets = [pet for pet in owner.pets]
+
+    if not pets:
+        response_body += f'<h2>Has no pets at this time.</h2>'
+
+    else:
+        for pet in pets:
+            response_body += f'<h2>Has pet {pet.species} named {pet.name}.</h2>'
+
+    response = make_response(response_body, 200)
+
+    return response
+    pass
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,44 @@
+'''remember this is a shebang operator which specifies the interpreter to be 
+used to run the script'''
 #!/usr/bin/env python3
 
 from random import choice as rc
+'''imports the choice function from the random moduel as alias rc'''
 
 from faker import Faker
+'''imports the faker class from the faker moduel'''
 
 from app import app
+
+'''imports the app instance from a moduel named app'''
 from models import db, Owner, Pet
+
+fake = Faker()
+'''creates an instance of the faker class to generate fake data'''
+#create an application context before we begin.  it will not necessarily be used
+#but will ensure that apps fail quickly if they are not configured with this context
+with app.app_context():
+    '''opens a flask application context'''
+
+    #deletion of all records handles as below vs vanilla: session.query(Model).delete()
+    Pet.query.delete()
+    Owner.query.delete()
+
+    owners = []
+    for n in range(50):
+        owner = Owner(name=fake.name())
+        owners.append(owner)
+
+    db.session.add_all(owners)
+
+    pets = []
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+    for n in range(100):
+        pet = Pet(name=fake.first_name(), species=rc(species), owner=rc(owners))
+        '''creates an instace of the Pet class with a randomly generated first name
+        using fake.first_name() method, a randomly select owner using the 
+        rc(owners) method and a randomly selected species using the rc(species) method'''
+        pets.append(pet)
+
+    db.session.add_all(pets)
+    db.session.commit()
